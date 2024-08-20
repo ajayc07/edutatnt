01f9e30bd751ae9e9a56558f83c0ec16d290f606
ajayc07 <ajaychandrasekaran8595@gmail.com>
2024-08-20 13:28:09 +0530

    Updating Fonts

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,11 @@ import './App.scss';
 
 function App() {
   return (
-    <></>
+    <>
+      <div>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, id ab cupiditate ea odio reprehenderit. Maiores pariatur voluptates esse beatae adipisci at consequatur qui minus, blanditiis sed sint iure quas, vero ratione! Vel quaerat dolores, libero temporibus ratione sapiente quia!
+      </div>
+    </>
   );
 }
 
